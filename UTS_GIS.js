ded1d1227699e261500deb54b93a5d3a1ab0ded7
DenjanD <alfien311@gmail.com>
2022-11-14 14:06:47 +0700

    Upload file JS Google Earth Engine

---
+++ UTS_GIS.js
@@ -0,0 +1,17 @@
+var point = ee.Geometry.Point([107.292, -7.7018]);
+
+var l8 = ee.ImageCollection('LANDSAT/LC08/C01/T1_TOA');
+
+var image = ee.Image(
+  l8.filterBounds(point)
+    .filterDate('2020-10-01', '2022-10-31')
+    .sort('CLOUD_COVER')
+    .first()
+);
+
+var nir = image.select('B5');
+var red = image.select('B4');
+var ndvi = nir.subtract(red).divide(nir.add(red)).rename('NDVI');
+
+var ndviParams = {min: -1, max: 1, palette: ['blue', 'white', 'green']};
+Map.addLayer(ndvi, ndviParams, 'NDVI image');
